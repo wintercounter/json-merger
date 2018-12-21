f3d42f97b34557eee458072378292e16fc666646
Viktor Vincze <viktor.vincze@doclerholding.com>
2018-12-21 14:44:26 +0100

    fix: set default strict value

--- dist/utils/types.js
+++ dist/utils/types.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var STRICT_TYPES = ['String', 'Array', 'Object', 'Number', 'Symbol', 'Boolean'];
 function isObject(value, isStrict) {
-    if (value && isStrict && STRICT_TYPES.indexOf(value.constructor.name) > -1) {
+    if (value && isStrict && STRICT_TYPES.indexOf(value.constructor.name) === -1) {
         return false;
     }
     return Object.prototype.toString.call(value).slice(8, -1) === "Object";
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,7 @@
 const STRICT_TYPES = ['String', 'Array', 'Object', 'Number', 'Symbol', 'Boolean'];
 
 export function isObject(value: any, isStrict?: boolean) {
-    if (value && isStrict && STRICT_TYPES.indexOf(value.constructor.name) > -1) {
+    if (value && isStrict && STRICT_TYPES.indexOf(value.constructor.name) === -1) {
         return false;
     }
     return Object.prototype.toString.call(value).slice(8, -1) === "Object";
